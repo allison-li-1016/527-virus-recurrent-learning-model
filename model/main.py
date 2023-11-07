5ead3c4707bfcb7c325b2faed8a560b29b914bb2
Allison Li <allli@uw.edu>
2023-11-06 23:17:13 -0800

    tried to implement train and eval

--- model/main.py
+++ model/main.py
@@ -1,30 +1,71 @@
 import itertools
 
-import CodonLoader from loader
-import RNNModel from model
+from loader import CodonLoader
+from model import RNNModel
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
 
 def main():
+    # load codon data for model
     path = "data/resulting-codons.txt"
     codon_loader = CodonLoader(path, test_split=0.2)
-    train_data = codon_loader.get_train_data() # (y)
+    train_data = codon_loader.get_train_data() 
     test_data = codon_loader.get_test_data()
 
+    # creating offset between x and y sequences
+    # so that each token is predicting the next token
+    train_x, train_y = parse_data(train_data)
+    test_x, test_y = parse_data(test_data)
+
     # Pretraining masking random codons
-    masked_codons = codon_loader.mask(train_data, 0.1)
+    # masked_codons = codon_loader.mask(train_data, 0.1)
 
-    # layer_types = ["LSTM", "GRU"]
-    # hidden_layer_sizes = [128, 256, 512]
+    # Define hyperparameters
+    n_epochs = 100
+    lr=0.01
 
+    # Grid search 
+    layer_types = ["LSTM", "GRU"]
+    hidden_layer_sizes = [128, 256, 512]
+    
+    for (lt, hls) in itertools.product(layer_types, hidden_layer_sizes):
+        # Define model 
+        model = RNNModel(lt, len(train_x), len(train_y), len(train_x[0]), hls)
+        # Define Loss, Optimizer
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        #Train model 
+        model.train(optimizer, criterion, n_epochs, train_x, train_y)
+        # Validation data
+        loss, accuracy, avg_loss= model.eval(model, optimizer, criterion, test_x, test_y)
 
-    # for (lt, hls) in itertools.product(layer_types, hidden_layer_sizes):
-    #     model = RNNModel(layer_type, hidder_layer_size, dropout_rate, learning_rate, epochs, batch_size)
-    #     # Validation data
-    #     acc = model.eval()
+        # Print stats
+        print(str(lt) + " accuracy: " + str(accuracy))
+        print(str(lt) + " loss: " + str(loss))
+    
+        # plotting avg-loss
+        plt.plot(avg_loss)
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        plt.title("Per-Epoch Losses-" + str(lt))
+        plt.savefig("avg-loss-" + str(lt)+".png")
+        plt.cla()
+        plt.clf()
 
+def parse_data(data):
+    # Creating lists that will hold our input and target sequences
+    x = []
+    y = []
 
-
-
-
+    for i in range(len(data)):
+        # Remove last character for input sequence
+        x.append(data[i][:-1])
+        
+        # Remove first character for target sequence
+        y.append(data[i][1:])
+    
+    return x, y
 
 if __name__ == "__main__":
     main()
--- model/model.py
+++ model/model.py
@@ -1,15 +1,16 @@
 import torch
 import torch.nn as nn
+
 # Seq to seq model
 class RNNModel:
     #would be nice to be able to pass in RNN type as parameter too
-    def __init__(self, input_size, output_size, hidden_dim, n_layers):
+    def __init__(self, layer_type, input_size, output_size, hidden_dim, n_layers):
         #super simple LSTM model 
         super(RNNModel, self).__init__()
         self.hidden_dim = hidden_dim
         self.n_layers = n_layers
         #defining layers
-        self.rnn = nn.LSTM(input_size, hidden_dim, n_layers)  
+        self.rnn = layer_type(input_size, hidden_dim, n_layers)  
         #fully connected layer -> connect network to output labels
         self.fc = nn.Linear(hidden_dim, output_size)
     
@@ -25,9 +26,53 @@ class RNNModel:
         
         # Reshaping the outputs such that it can be fit into the fully connected layer
         # -1 is n of original output tensor, second dimension is the same as the hidden layer
+
+        #this might need to be deleted if its many to many
         out = out.contiguous().view(-1, self.hidden_dim)
         out = self.fc(out)
 
         # return output layer and hidden state (for training and RNN optimization)
         return out, hidden 
+
+    def train(self,optimizer, criterion, epochs,x,y):
+        epoch_losses = []
+        for epoch in range(1, epochs + 1):
+            optimizer.zero_grad() # Clears existing gradients from previous epoch
+            output, hidden = self.forward(x)
+            loss = criterion(output, y.view(-1).long())
+            loss.backward() # Does backpropagation and calculates gradients
+            epoch_losses.append(loss.item())
+            optimizer.step() # Updates the weights accordingly
+
+            # Calculate accuracy
+            predicted_labels = torch.argmax(output, dim=1)
+            correct_predictions = (predicted_labels == y).sum().item()
+            total_samples = y.size(0)
+            accuracy = correct_predictions / total_samples
+            
+            if epoch%10 == 0:
+                print('Epoch: {}/{}.............'.format(epoch, epochs), end=' ')
+                print("Loss: {:.4f}, Accuracy: {:.2f}%".format(loss.item(), accuracy * 100))
+        
+        return epoch_losses
+    
+    def eval(self,optimizer, criterion, epochs, x, y):
+        # train and get per epoch losses
+        avg_loss = self.train(optimizer, criterion, epochs, x, y)
+        # accuracy and loss on test set
+        test_loss = 0.0
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for x_val, y_val in zip(x,y):
+                outputs = self.forward(x_val)
+                loss = criterion(x_val,y_val)
+                test_loss += loss.item()
+                _, predicted = torch.argmax(outputs, dim=1)
+                total += y.size(0)
+                correct += (predicted == y_val).sum().item()
+            test_loss /= len(x)
+            test_accuracy = correct / total
+
+        return test_loss, test_accuracy, avg_loss
 
